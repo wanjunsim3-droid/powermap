1e7807e9e1a19d427a2bf04b2c36b9ccfabe711f
simwanjun <wanjunsim3@gmail.com>
2026-09-15 16:10:50 +0900

    fix: Update base path to relative in vite config

--- 파티룸/vite.config.js
+++ 파티룸/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: './',
   publicDir: '.', // 정적 자원 디렉터리를 프로젝트 루트로 설정하여 images 폴더에 바로 접근 가능하게 함
   build: {
     rollupOptions: {
